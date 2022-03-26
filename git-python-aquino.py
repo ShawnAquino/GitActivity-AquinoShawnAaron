b1d10dbab6837e52f5ae5b13ab1178bd65b6c857
AquinoShawnAaron <oniuqa773@gmail.com>
2022-03-26 14:16:14 +0800

    My Educational Background added

--- git-python-aquino.py
+++ git-python-aquino.py
@@ -4,3 +4,12 @@ def intro ():
     print ("Birthday: January 19, 2002")
 
 intro()
+
+def education ():
+    print ("College: Marinduque State College")
+    print ("Course: BSIT")
+    print ("Year: 2nd Year")
+    print ("Section: 2F")
+
+education()
+
